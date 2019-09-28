53b7964a32efb0b8641c9634404acde797b10796
jojo20872 <49132464+jojo20872@users.noreply.github.com>
2019-09-28 17:12:12 -0400

    Second commit. Added map and button.

--- app/src/main/java/com/example/spot_itgarbage/MapsActivity.java
+++ app/src/main/java/com/example/spot_itgarbage/MapsActivity.java
@@ -40,8 +40,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng UMBC = new LatLng(39.2520576, -76.707197);
+        mMap.addMarker(new MarkerOptions().position(UMBC).title("Marker in UMBC"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(UMBC));
     }
 }
+
