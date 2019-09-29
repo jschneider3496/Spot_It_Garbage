e254bad7d4e440f39995f57fca2e35a0ee77433d
jojo20872 <49132464+jojo20872@users.noreply.github.com>
2019-09-29 07:01:00 -0400

    Added marker functionality

--- app/src/main/java/com/example/spot_itgarbage/MapsActivity.java
+++ app/src/main/java/com/example/spot_itgarbage/MapsActivity.java
@@ -36,8 +36,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import java.util.ArrayList;
 
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
-
-
+    //TODO: gello
     private static DatabaseReference Database;
     private static final String TAG = "MapActivity";
     private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
@@ -73,11 +72,6 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
 
     }
-
-
-
-
-
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -119,26 +113,6 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
             }
         });
 
-//        Database.addValueEventListener(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-//
-//
-//                for(DataSnapshot ds : dataSnapshot.getChildren()){
-//                    MarkerData value = dataSnapshot.getValue(MarkerData.class);
-//                    Log.d(TAG, "Value is: " + value);
-//                    addMarker(value.getDesc(), value.getLat(), value.getLng(), value.getRating(), dataSnapshot.getKey());
-//                }
-//
-//
-//            }
-//
-//            @Override
-//            public void onCancelled(@NonNull DatabaseError databaseError) {
-//                Log.w(TAG, "Failed to read value.", databaseError.toException());
-//            }
-//        });
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
@@ -147,7 +121,6 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         mapFragment.getMapAsync(this);
 
         getLocationPermission();
-
 
         ImageButton imageButton = findViewById(R.id.imageButton1);
 
@@ -165,9 +138,6 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                 intent.putExtras(location);
                 startActivity(intent);
 
-//                addMarker();
-
-
             }
         });
     }
@@ -181,9 +151,6 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     }
 
-    private void removeMarker() {
-
-    }
 
     public void openTrashInput() {
         Intent intent = new Intent(this, TrashInput.class);
@@ -212,7 +179,6 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
                             setLat(currentLocation.getLatitude());
                             setLng(currentLocation.getLongitude());
 
-//                            latLngList.add(new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude()));
 
                         } else {
                             Log.d(TAG, "onComplete: current location is null");
@@ -306,64 +272,3 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     }
 }
 
-//public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
-//
-//
-////
-//
-//    private GoogleMap mMap;
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_maps);
-//        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-//        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-//                .findFragmentById(R.id.map);
-//        mapFragment.getMapAsync(this);
-//
-//        ImageButton imageButton = findViewById(R.id.imageButton1);
-//
-//        imageButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                openTrashInput();
-//            }
-//        });
-//    }
-//
-//    public void openTrashInput() {
-//        Intent intent = new Intent(this, TrashInput.class);
-//        startActivity(intent);
-//    }
-//
-//
-//    /**
-//     * Manipulates the map once available.
-//     * This callback is triggered when the map is ready to be used.
-//     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-//     * we just add a marker near Sydney, Australia.
-//     * If Google Play services is not installed on the device, the user will be prompted to install
-//     * it inside the SupportMapFragment. This method will only be triggered once the user has
-//     * installed Google Play services and returned to the app.
-//     */
-//    @Override
-//    public void onMapReady(GoogleMap googleMap) {
-//        mMap = googleMap;
-//
-//        // Add a marker in Sydney and move the camera
-//        LatLng UMBC = new LatLng(39.2520576, -76.707197);
-//        mMap.addMarker(new MarkerOptions().position(UMBC).title("Marker in UMBC"));
-//        mMap.moveCamera(CameraUpdateFactory.newLatLng(UMBC));
-//    }
-//}
-
-//import com.google.android.gms.location.FusedLocationProviderClient;
-//        import com.google.android.gms.location.LocationServices;
-//        import com.google.android.gms.maps.CameraUpdateFactory;
-//        import com.google.android.gms.maps.GoogleMap;
-//        import com.google.android.gms.maps.OnMapReadyCallback;
-//        import com.google.android.gms.maps.SupportMapFragment;
-//        import com.google.android.gms.maps.model.LatLng;
-//        import com.google.android.gms.tasks.OnCompleteListener;
-//        import com.google.android.gms.tasks.Task;
