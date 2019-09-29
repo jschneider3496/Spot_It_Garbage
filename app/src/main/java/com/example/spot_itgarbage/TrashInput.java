8476e8a1f4333d863c1e3335988ba5dedd2dcf0c
jojo20872 <49132464+jojo20872@users.noreply.github.com>
2019-09-28 23:36:05 -0400

    Added access to phone camera

--- app/src/main/java/com/example/spot_itgarbage/TrashInput.java
+++ app/src/main/java/com/example/spot_itgarbage/TrashInput.java
@@ -2,7 +2,13 @@ package com.example.spot_itgarbage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -11,11 +17,32 @@ public class TrashInput extends AppCompatActivity {
     private static SeekBar seekbar;
     private static TextView textview;
 
+    ImageView imageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trash_input);
         seekBar();
+        ImageButton imageButton = (ImageButton) findViewById(R.id.imageButton);
+        imageView = (ImageView) findViewById(R.id.imageView);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 0);
+            }
+
+                                       }
+        );
+
+    }
+
+
+    protected void onActivity(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+        imageView.setImageBitmap(bitmap);
     }
 
     public void seekBar() {
