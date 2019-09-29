d65d04556cdc6b38f6e734abc123b98af0699728
jojo20872 <49132464+jojo20872@users.noreply.github.com>
2019-09-29 11:33:58 -0400

    Added marker functionality

--- app/src/main/java/com/example/spot_itgarbage/TrashInput.java
+++ app/src/main/java/com/example/spot_itgarbage/TrashInput.java
@@ -1,7 +1,9 @@
 package com.example.spot_itgarbage;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
@@ -11,11 +13,22 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class TrashInput extends AppCompatActivity {
     private static SeekBar seekbar;
@@ -24,8 +37,12 @@ public class TrashInput extends AppCompatActivity {
     private static DatabaseReference Database;
     private static EditText editText;
 
+    private StorageReference storageRef;
+    private FirebaseStorage firebaseStorage;
     private static ImageView imageView;
+    private ProgressDialog progressDialog;
     private static final String TAG = "TrashInput";
+    private Uri ImageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +54,11 @@ public class TrashInput extends AppCompatActivity {
         editText = (EditText) findViewById(R.id.editText);
         Database = FirebaseDatabase.getInstance().getReference();
 
+        firebaseStorage = FirebaseStorage.getInstance();
+        ImageUri = null;
+        storageRef = FirebaseStorage.getInstance().getReference();
+
+        progressDialog = new ProgressDialog(this);
 
         // SUBMIT BUTTON
         Button submit = (Button) findViewById(R.id.button2);
@@ -46,7 +68,7 @@ public class TrashInput extends AppCompatActivity {
                                            public void onClick(View view) {
                                                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                                                startActivityForResult(intent, 0);
-
+//                                               dispatchTakePictureIntent();
 
                                            }
 
@@ -56,6 +78,8 @@ public class TrashInput extends AppCompatActivity {
         submit.setOnClickListener(new View.OnClickListener() {
                                       @Override
                                       public void onClick(View view) {
+
+                                          //postPic();
                                           String description = editText.getText().toString().trim();
                                           int rating = seekbar.getProgress();
 
@@ -68,21 +92,132 @@ public class TrashInput extends AppCompatActivity {
                                           MarkerData dataEntry = new MarkerData(description, rating, lat, lng);
                                           Database.push().setValue(dataEntry);
 
-
                                       }
                                   }
         );
-
-
     }
-
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         Bitmap bitmap = (Bitmap) data.getExtras().get("data");
         imageView.setImageBitmap(bitmap);
+        upload(bitmap);
+//        ImageUri = data.getData();
+
     }
 
+
+    private void upload(Bitmap photo) {
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        photo.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+
+        byte[] b = stream.toByteArray();
+        StorageReference storageReference =FirebaseStorage.getInstance().getReference().child("documentImages").child(new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date()));
+        //StorageReference filePath = FirebaseStorage.getInstance().getReference().child("profile_images").child(userID);
+        storageReference.putBytes(b).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                String downloadUrl = taskSnapshot.getMetadata().getReference().getDownloadUrl().toString();
+                Toast.makeText(TrashInput.this, "uploaded", Toast.LENGTH_SHORT).show();
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(TrashInput.this,"failed",Toast.LENGTH_LONG).show();
+
+
+            }
+        });
+    }
+
+//    private void postPic() {
+//        System.out.println("Uploading to Cloud...");
+//
+//        StorageReference filePath = storageRef.child("Trash_Images/" + UUID.randomUUID().toString());
+//
+//        filePath.putFile(ImageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+//            @Override
+//            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+//                String downloadUrl = taskSnapshot.getMetadata().getReference().getDownloadUrl().toString();
+//            }
+//        });
+  //  }
+
+//    static final int REQUEST_TAKE_PHOTO = 1;
+//    String currentPhotoPath;
+//
+//    private File createImageFile() throws IOException {
+//        // Create an image file name
+//        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+//        String imageFileName = "JPEG_" + timeStamp + "_";
+//        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+//        File image = File.createTempFile(
+//                imageFileName,  /* prefix */
+//                ".jpg",         /* suffix */
+//                storageDir      /* directory */
+//        );
+//
+//        // Save a file: path for use with ACTION_VIEW intents
+//        currentPhotoPath = image.getAbsolutePath();
+//        return image;
+//    }
+//    Uri imgUri;
+//    private void dispatchTakePictureIntent() {
+//        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//        // Ensure that there's a camera activity to handle the intent
+//        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+//            // Create the File where the photo should go
+//            File photoFile = null;
+//            try {
+//                photoFile = createImageFile();
+//            } catch (IOException ex) {
+//                // Error occurred while creating the File
+//            }
+//            // Continue only if the File was successfully created
+//            if (photoFile != null) {
+//                Uri imgUri = FileProvider.getUriForFile(this,
+//                        "com.example.spot_itgarbage",
+//                        photoFile);
+//                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, imgUri);
+//                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+//            }
+//        }
+//    }
+//    private String getExtension(Uri uri) {
+//        ContentResolver cr = getContentResolver();
+//        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+//        return mimeTypeMap.getExtensionFromMimeType(cr.getType(uri));
+//    }
+//    private void fileUploader() {
+//        StorageReference Ref = storageRef.child(System.currentTimeMillis()+ "." + getExtension(imgUri));
+//
+//        Ref.putFile(imgUri)
+//                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+//                    @Override
+//                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+//                        // Get a URL to the uploaded content
+//                      //  Uri downloadUrl = taskSnapshot.getDownloadUrl();
+//                        Toast.makeText(TrashInput.this, "Image uploaded", Toast.LENGTH_LONG).show();
+//                    }
+//                })
+//                .addOnFailureListener(new OnFailureListener() {
+//                    @Override
+//                    public void onFailure(@NonNull Exception exception) {
+//                        // Handle unsuccessful uploads
+//                        // ...
+//                    }
+//                });
+//    }
+//public void encodeBitmapAndSaveToFirebase(Bitmap bitmap) {
+//    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+//    bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
+//    String imageEncoded = Base64.encodeToString(baos.toByteArray(), Base64.DEFAULT);
+//    DatabaseReference ref = FirebaseDatabase.getInstance();
+//
+//    ref.setValue(imageEncoded);
+//}
 
     public void seekBar() {
         seekbar = (SeekBar) findViewById(R.id.seekBar);
